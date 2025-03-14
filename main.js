47717cf080a56a6c2215c16f79835674fd65c2df
ZEROES_07 <40240302@esmad.ipp.pt>
2025-03-14 12:43:05 +0000

    fix

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ let nota;
 
 function adicionarNota() {
     nota = document.getElementById("inserirNota").value;
-    document.getElementById("notas").innerHTML += `<p class="${notaClass}">${nota}</p>`
+    document.getElementById("notas").innerHTML += `<p>${nota}</p>`
 }
