e610c211a92ac3506be7062e86931715f18d682c
ZEROES_07 <40240302@esmad.ipp.pt>
2025-03-19 12:11:49 +0000

    euromilhoes

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let star1 = Math.floor(Math.random() * 12) + 1;
 let star2 = Math.floor(Math.random() * 12) + 1;
 
 document.getElementById("add").innerHTML=`<p>${num1} ${num2} ${num3} ${num4} ${num5} ${star1} ${star2}</p>`
-document.getElementById("add").innerHTML+="<button onclick='randomAgain()'>Outra vez</button>"
+document.getElementById("add").innerHTML+="<button class='btn btn-primary' onclick='randomAgain()'>Outra vez</button>"
 
 function randomAgain() {
     num1 = Math.floor(Math.random() * 50) + 1;
@@ -18,5 +18,5 @@ function randomAgain() {
     star1 = Math.floor(Math.random() * 12) + 1;
     star2 = Math.floor(Math.random() * 12) + 1;
     document.getElementById("add").innerHTML=`<p>${num1} ${num2} ${num3} ${num4} ${num5} ${star1} ${star2}</p>`
-    document.getElementById("add").innerHTML+="<button onclick='randomAgain()'>Outra vez</button>"
+    document.getElementById("add").innerHTML+="<button class='btn btn-primary' onclick='randomAgain()'>Outra vez</button>"
 }
