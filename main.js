8dfbec9639ad4c8d2088198bb9c7cf3832df8c46
ZEROES_07 <40240302@esmad.ipp.pt>
2025-03-19 12:02:23 +0000

    euromilhoes

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-let nota;
+let num1 = Math.floor(Math.random() * 50) + 1;
+let num2 = Math.floor(Math.random() * 50) + 1;
+let num3 = Math.floor(Math.random() * 50) + 1;
+let num4 = Math.floor(Math.random() * 50) + 1;
+let num5 = Math.floor(Math.random() * 50) + 1;
+let star1 = Math.floor(Math.random() * 12) + 1;
+let star2 = Math.floor(Math.random() * 12) + 1;
 
+document.getElementById("add").innerHTML=`<p>${num1} ${num2} ${num3} ${num4} ${num5} ${star1} ${star2}</p>`
+document.getElementById("add").innerHTML+="<button onclick='randomAgain()'>Outra vez</button>"
 
-function adicionarNota() {
-    nota = document.getElementById("inserirNota").value;
-    document.getElementById("notas").innerHTML += `<p>${nota}</p>`
+function randomAgain() {
+    num1 = Math.floor(Math.random() * 50) + 1;
+    num2 = Math.floor(Math.random() * 50) + 1;
+    num3 = Math.floor(Math.random() * 50) + 1;
+    num4 = Math.floor(Math.random() * 50) + 1;
+    num5 = Math.floor(Math.random() * 50) + 1;
+    star1 = Math.floor(Math.random() * 12) + 1;
+    star2 = Math.floor(Math.random() * 12) + 1;
+    document.getElementById("add").innerHTML=`<p>${num1} ${num2} ${num3} ${num4} ${num5} ${star1} ${star2}</p>`
+    document.getElementById("add").innerHTML+="<button onclick='randomAgain()'>Outra vez</button>"
 }
